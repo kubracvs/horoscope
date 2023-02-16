64688b4d91e7679a4afbfd04a2b54601603593a8
kubra <kubracavuss@gmail.com>
2023-02-16 21:22:02 +0300

    first commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import "./Header.scss"
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1 className="text-shadow">who are you?</h1>
+    </div>
+  )
+}
+
+export default Header
